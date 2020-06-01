f140ef679b56c86e2c32bc3426b8367ed2c20a7d
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-06-01 14:29:23 -0700

    Add title to index.html, add business logic for subtract, multiply and divide to scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 function add(number1, number2) {
-return number1 + number2;
+  return number1 + number2;
+};
+
+function subtract(number1, number2) {
+  return number1 - number2;
+};
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+
+function divide(number1, number2) {
+  return number1 / number2;
 };
 
 let number1 = parseInt(prompt('Enter a number:'));
 let number2 = parseInt(prompt('Enter another number:'));
 
-alert(add(number1, number2));
+alert(divide(number1, number2));
